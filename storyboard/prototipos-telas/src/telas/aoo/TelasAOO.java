5068c15581b3214e150e5466c36efceb1ce1aeb5
João Henrique <37977379+mannuscritto@users.noreply.github.com>
2019-05-22 14:23:57 -0300

    Primeiro commit
    Os arquivos foram organizados e renomeados.

---
+++ storyboard/prototipos-telas/src/telas/aoo/TelasAOO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telas.aoo;
+
+import view.telaLogin;
+
+/**
+ *
+ * @author Lucas Santos
+ */
+public class TelasAOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        telaLogin tl = new telaLogin();
+        tl.setVisible(true);
+    }
+    
+}
